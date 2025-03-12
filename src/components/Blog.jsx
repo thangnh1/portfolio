b92650e5bde45434dc2f2e6c6d3a2ccc36c8163b
beebongbenh <thangnh.info@gmail.com>
2025-03-12 13:40:50 +0700

    update

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -25,7 +25,7 @@ const Blog = () => {
         variants={fadeIn("right", "", 0.5, 1)}
         className="xs:bottom-10 bottom-32 w-full flex justify-center items-center py-8"
       >
-        <img src="src\assets\blog.png" alt="Blog" />
+        <img src="src/assets/blog.png" alt="Blog" />
       </motion.div>
       <motion.div
         variants={fadeIn("right", "", 0.5, 1)}
